de670daf73c71e6b92bd9ad0c98b3c0bc6b53b48
Karl veggerby <karlveggerby@karls-personal-macbookpro13.lan>
2023-02-07 09:17:46 -0800

    made reprex for assignement

--- R/01_summary_stats.R
+++ R/01_summary_stats.R
@@ -2,15 +2,18 @@
 
 ## load libraries
 library("here")
-
+library(reprex)
 ## set location of the data directory 
 data_dir <- here("data")
 
 ## load data file
 pisaster_data <- readRDS(here(data_dir, "pisaster_data.Rds"))
 
-## peek at the data
 head(pisaster_data)
+str(pisaster_data)
 
-## calculate mean counts across all years, sites, and plots
+
 mean_count <- mean(pisaster_data$count)
+
+print(pisaster_data)
+head(pisaster_data)
